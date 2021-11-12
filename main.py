168a6bd062a9ca33d2205566debb2809ad54a5e1
Ghulam <inferno2960@gmail.com>
2021-11-12 18:54:13 -0500

    added logging, using env var for bot token, adde member intents, , added a method to create a db and tables for it, and updated the way cogs are loaded

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
 import os
+import sqlite3
+import logging
 import discord 
+from dotenv import load_dotenv
 from discord.ext import commands 
 
 
-bot = commands.Bot(command_prefix="!")
+# set up logging for the bot 
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter((logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')))
+logger.addHandler(handler)
 
+# load enviroment vars 
+load_dotenv() 
+TOKEN = os.getenv("TOKEN")
+
+# set up member intents and the command prefix for the bot 
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+async def initialize():
+    await bot.wait_until_ready()
+
+    db = sqlite3.connect('ryu.db')
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS economyTable (guild_id int, user_id int, job text, wallet int, bank int, PRIMARY KEY (guild_id, user_id))")
 
 
 if __name__ == '__main__':
-    for ext in os.listdir("./cogs"):
-        bot.load_extension(ext)
+    for filename in os.listdir("./cogs"):
+        if filename.endswith('.py'):
+            try: 
+                cog_loaded = filename.replace(".py", "")
+                bot.load_extension(f'cogs.{filename[:-3]}')
+                print(cog_loaded)
+            except Exception as e: 
+                print(e)
 
-bot.run("ODkyOTU2MTY4MDE4ODc4NTA2.YVUcPA.6P3tdg3vQAuo1ppRqVETuHaDCsY")
+bot.loop.create_task(initialize())
+bot.run(TOKEN)
