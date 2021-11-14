dca4fac847b49ec10df09bc1b1717763f074b550
Ghulam <inferno2960@gmail.com>
2021-11-14 18:29:38 -0500

    added a way to check level

--- cogs/Level.py
+++ cogs/Level.py
@@ -1,5 +1,6 @@
 import random
 from discord.ext import commands
+import discord
 
 class Level(commands.Cog):
 
@@ -72,6 +73,29 @@ class Level(commands.Cog):
                     await message.channel.send(f'{message.author.mention} Congrats!! You are now level **{new_level}!!!**')
             await self.bot.db.commit()
 
+
+    @commands.command()
+    async def level(self, ctx, member: discord.Member = None): 
+        if member is None: 
+            member = ctx.author
+
+        # FETCH DATA FROM DB
+        async with self.bot.db.execute('SELECT exp, lvl, msg_count  FROM levelData WHERE guild_id = ? AND user_id = ?', (ctx.guild.id, member.id)) as cursor: 
+            data = await cursor.fetchone()
+            curr_exp = data[0] 
+            lvl = data[1] 
+            msg_count = data[2]
+        
+        exp_to_next_level = int((.04 * (lvl ** 3) + .8 * (lvl ** 2) + 2 * lvl) + 100) 
+        # embed that shows information about the level stats of the requwsted user 
+        embed = discord.Embed(title='Level Stats', description=f'Level stats for the server {ctx.guild.name}', color=member.color)
+        embed.set_author(name=f'{ctx.author.name}#{ctx.author.discriminator}', icon_url=member.avatar_url)
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.add_field(name='Amount of messages sent: ', value=msg_count, inline=False)
+        embed.add_field(name='Current Level:', value=f'**{lvl}**', inline=True)
+        embed.add_field(name='**Experience**: ', value=f'{curr_exp}/ {exp_to_next_level}')
+
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Level(bot)) 
