70fd2c56a60255a55eb3352909873b395d3916f2
Ghulam <inferno2960@gmail.com>
2021-11-14 19:41:32 -0500

    added a function to generate a quote

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -1,11 +1,22 @@
 from discord.ext import commands
 import discord
+import requests
+import random 
 
 
 class Misc(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot 
+        self.list_of_qoutes = []
+
+        r = requests.get('https://type.fit/api/quotes')
+        data = r.json()
+
+        for i in data:
+            self.list_of_qoutes.append(i)
+
+        
 
     @commands.command()
     async def show_avatar(self, ctx, *, member: discord.Member = None):
@@ -29,6 +40,13 @@ class Misc(commands.Cog):
             )
             show_av.set_image(url=f'{member.avatar_url}')
             await ctx.send(embed=show_av)
+
+
+    @commands.command(aliases=['quote', 'randomquote', 'qu'], help='Responds with a random quote')
+    async def quote_gen(self, ctx):
+        random_index = random.randint(0, (len(self.list_of_qoutes) - 1))
+        quote = f'"{self.list_of_qoutes[random_index]["text"]}" - {self.list_of_qoutes[random_index]["author"]}'
+        await ctx.send(quote)
 
 
     @commands.command(name='ping', help='Shows the ping.')
