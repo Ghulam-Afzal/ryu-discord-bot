d5ebea54bcbf835b16c810f86b72753f578dd1c8
Ghulam <inferno2960@gmail.com>
2021-11-14 19:34:22 -0500

    added a ping command

--- cogs/Misc.py
+++ cogs/Misc.py
@@ -30,5 +30,11 @@ class Misc(commands.Cog):
             show_av.set_image(url=f'{member.avatar_url}')
             await ctx.send(embed=show_av)
 
+
+    @commands.command(name='ping', help='Shows the ping.')
+    async def show_ping(self, ctx):
+        p = round(self.bot.latency, 4) * 1000
+        await ctx.send(f'{p}ms')
+
 def setup(bot):
     bot.add_cog(Misc(bot))
