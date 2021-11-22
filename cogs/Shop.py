53f01ab2a1c5d257fca9291417ce345c0c9d92a5
Ghulam <inferno2960@gmail.com>
2021-11-22 17:22:04 -0500

    added a function to list the items that can be purchased

--- cogs/Shop.py
+++ cogs/Shop.py
@@ -1,20 +1,29 @@
+from discord import embeds
 from discord.ext import commands
-
+import discord 
 
 class Shop(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot 
+        self.shop = {"car": 30000, "house": 450000, "cat": 5000, "dog": 5000, "mansion": 1000000} 
         
         
         
     @commands.command(name="plist")
     async def list_purchasble_items(self, ctx):
-        pass 
+         
+        embed = discord.Embed(title="SHOP")
+         
+        for item in self.shop:
+            embed.add_field(name=f"{item}", value=f"The price of this item is {self.shop[item]}", inline=False)
+             
+        await ctx.send(embed=embed)
+        return 
     
     @commands.command()
     async def purchase(self, ctx, *, item):
-        pass 
+        pass
     
     
 def setup(bot):
