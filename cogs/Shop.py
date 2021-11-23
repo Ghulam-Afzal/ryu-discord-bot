df65b474e0a57e20f0ce8eb57ccc3549e921d216
Ghulam <inferno2960@gmail.com>
2021-11-23 16:05:25 -0500

    changed structure of the shopData table

--- cogs/Shop.py
+++ cogs/Shop.py
@@ -23,7 +23,31 @@ class Shop(commands.Cog):
     
     @commands.command()
     async def purchase(self, ctx, *, item):
-        pass
+        if item is None: 
+            await ctx.send("A item to purchase needs to be specified.")
+            # TODO: make the table have multiple columns where that use a  true false value to show whether the person owns something or not. 
+        else: 
+            if item in self.shop:
+                cursor = await self.bot.db.execute("SELECT items WHERE guild_id = ? AND user_id = ?", (ctx.guild.id, ctx.author.id))
+                result = await cursor.fetchone()
+                
+                if result is None:
+                    lst = []
+                    lst.append(item)
+                    temp = str(lst)
+                    await self.bot.db.execute("UPDATE items WHERE guild_id = ? AND user_id = ?", (temp, ctx.guild.id, ctx.author.id))        
+                
+                else:
+                    temp1 = list(result[0])
+                    temp1.append(item)
+                    temp2 = str(temp1)
+                    await self.bot.db.execute("UPDATE items WHERE guild_id = ? AND user_id = ?", (temp2, ctx.guild.id, ctx.author.id))
+                    return 
+                
+                await ctx.send(f"You have purchased {item}")   
+                return 
+            
+            else: await ctx.send(f"{item} is not up for purchase.")  
     
     
 def setup(bot):
